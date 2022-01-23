ed0719e526101a3205889d5218e81e5c210982a3
unknown <lynv.dev@gmail.com>
2022-01-23 17:39:04 +0700

    js homework1

---
+++ js/js_homework/checked_integer.js
@@ -0,0 +1,15 @@
+//1: basic
+// function checkInteger(value){
+//     return (Number.isInteger(value) && value > 0);
+// }
+
+// console.log(checkInteger(1)); // true
+// console.log(checkInteger(1.2)); // false
+// console.log(checkInteger(3)); // true
+// console.log(checkInteger(0)); // false
+//2: arrow func
+let checkedInteger = (value) =>{
+    return (Number.isInteger(value) && value > 0)
+};
+
+console.log(checkedInteger(4))
